f99662fd798ab6a04ee2b752ff474998c0a04adf
Ömer Asaf Karasu <omerconsept999@gmail.com>
2023-08-13 22:05:45 +0300

    Home Page Soon text added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Menu from '@/components/menu/menu';
+import Header from '@/components/header/header';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div className="w-full h-screen bg-black">
       <Menu page="home" />
+      <Header />
     </div>
   );
 }
