400890a45bb1558ad63ae08565ca986f67d7fa08
Ömer Asaf Karasu <omerconsept999@gmail.com>
2023-08-13 18:41:54 +0300

    Menu Created

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Menu from '@/components/menu/menu';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="w-full h-screen bg-black">
+      <Menu page="home" />
     </div>
   );
 }
